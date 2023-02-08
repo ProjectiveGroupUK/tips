fee0c78a79d09d10a21087baeb3956fe30789d31
Sam Foltin <sam.foltin@dtsquared.co.uk>
2023-02-08 14:42:35 +0000

    Introduce pages directory with processes page, and add state variable enums

---
+++ tips/app/enums.py
@@ -0,0 +1,12 @@
+class StateVariable:
+    ENTRY_POINT = 'entryPoint'
+    DISPLAY_JSON = 'display_json'
+    PROCESS_LOG_RESULT_1 = 'process_log_result1'
+    PROCESS_LOG_RESULT_2 = 'process_log_result2'
+    FETCH_BUTTON_CLICKED = 'fetchButtonClicked'
+
+class EntryPoint:
+    LOG_LIST = 'LogList'
+    PROCESS_LIST = 'ProcessList'
+    PROCESS_ADD = 'ProcessAdd'
+    SAVE_PROCESS = 'SaveProcess'
