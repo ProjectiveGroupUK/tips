10de27218435b138854fb80a3124a1b37ac58114
Sam Foltin <sam.foltin@dtsquared.co.uk>
2023-03-02 09:32:00 +0000

    Update process and command interfaces to match removal of NVL function from fetching SQL query

--- tips/app/components/src/interfaces/Interfaces.d.ts
+++ tips/app/components/src/interfaces/Interfaces.d.ts
@@ -1,7 +1,7 @@
 export declare interface ProcessDataInterface extends Array <{
     id: number;
     name: string;
-    description: string;
+    description: string | null;
     steps: Array<CommandDataInterface>;
     status: 'active' | 'inactive';
 }> {};
@@ -9,20 +9,20 @@ export declare interface ProcessDataInterface extends Array <{
 export declare interface CommandDataInterface {
     PROCESS_CMD_ID: number;
     CMD_TYPE: 'APPEND' | 'COPY_INTO_FILE' | 'DELETE'| 'DI' | 'MERGE' | 'OI' | 'PUBLISH_SCD2_DIM' | 'REFRESH' | 'TI' | 'TRUNCATE';
-    CMD_SRC: string;
+    CMD_SRC: string | null;
     CMD_TGT: string;
-    CMD_WHERE: string;
-    CMD_BINDS: string;
+    CMD_WHERE: string | null;
+    CMD_BINDS: string | null;
     REFRESH_TYPE: 'DI' | null;
-    BUSINESS_KEY: string;
-    MERGE_ON_FIELDS: string;
-    GENERATE_MERGE_MATCHED_CLAUSE: 'Y' | '';
-    GENERATE_MERGE_NON_MATCHED_CLAUSE: 'Y' | '';
-    ADDITIONAL_FIELDS: string;
+    BUSINESS_KEY: string | null;
+    MERGE_ON_FIELDS: string | null;
+    GENERATE_MERGE_MATCHED_CLAUSE: 'Y' | null;
+    GENERATE_MERGE_NON_MATCHED_CLAUSE: 'Y' | null;
+    ADDITIONAL_FIELDS: string | null;
     TEMP_TABLE: 'Y' | null;
     CMD_PIVOT_BY: string | null;
     CMD_PIVOT_FIELD: string | null;
-    DQ_TYPE: 'DUPS' | 'SCD2' | '';
-    CMD_EXTERNAL_CALL: string;
-    ACTIVE: 'Y' | 'N';
+    DQ_TYPE: 'DUPS' | 'SCD2' | null;
+    CMD_EXTERNAL_CALL: string | null;
+    ACTIVE: 'Y' | 'N' | null;
 }
